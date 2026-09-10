8d510da362823b43bb27d7bb7949bddb4fec87cb
anujdhungana217 <anujdhungana217@gmail.com>
2026-09-10 10:19:45 +0545

    Backend all code

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
 import express from "express";
 import multer from "multer";
 import UploadFile from "./services/storage.service.js";
-
+import PostModel from "./modles/data.model.js";
 
 
 const app = express();
 const upload=multer({storage:multer.memoryStorage()})
 
 app.post("/create-post",upload.single("photos"),async(req,res)=>{
-    const data=req.body
     const result= await UploadFile(req.file.buffer)
-    // console.log(data);
+    // console.log(req.body);
     // console.log(req.file);
-    
-    console.log(result);
-    
+    // console.log(result);
+const  post=await PostModel.create({
+    photo:result.url,
+    caption:req.body.caption
+})
+    return res.status(201).json({
+        message:"Post Created Successfully",
+        post
+    })
 
+})
+
+app.get("/create-post",async(req,res)=>{
+    const post=await PostModel.find()
+    res.status(200).json({
+        message:"Fetch Post Successfully",
+        post
+    })
 })
 
 export default app;
